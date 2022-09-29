44efcd69391ad5505a7a1d00bd5b9c8ee933e1ed
Robson Luan <robsonluan273@hotmail.com>
2022-09-29 10:50:19 -0300

    Added one more app

--- app9.js
+++ app9.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import {writeFileSync} from 'fs'
 
 let req = axios.create({
     baseURL: 'http://arest.me/api',
@@ -10,22 +11,24 @@ async function main(){
         let data = await req.get('sites')
         let reqs = []
         for(let i in data.data){
-            if(i == 10){
+            if(i == 3){
                 break
-            } else if (i <= 5){
-                continue
             }
-
             reqs.push(new Promise(async (resolve, reject) => {
                 try{
                     let data1 = await req.get(`sites/${data.data[i].id}`)
-                    resolve((await req.get(`regions/${data1.data.regionName}`)).data)
+                    //console.log(data1.data)
+                    if (data1.data.regionName)
+                        resolve((await req.get(`regions/${data1.data.regionName}`)).data)
+                    else
+                        resolve(`ERROR! The location ${data1.data.name} has no region specified.`)
                 } catch(err){
                     reject(err)
                 }
             }))
         }
         let data2 = await Promise.all(reqs)
+        writeFileSync('Output.json', JSON.stringify(data2))
         console.log(data2)
     } catch(err){
         console.log(err)
